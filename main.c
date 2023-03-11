a401a1e6afab1533db243748c9826fe6332f48bb
zvizzct <victor.puerta01@estudiant.upf.edu>
2023-03-11 18:51:01 +0100

    almost done

--- main.c
+++ main.c
@@ -6,164 +6,53 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "src/utilsFiles.h"
+#include "src/utilsFiles.c"
+#include "src/stack.h"
+#include "src/actionTable.c"
+#include "src/gotoTable.c"
 
-FILE *file;
-char *fileContent;
-
-char *setUpfileContent(char *fileName)
+int main(int argc, char const *argv[])
 {
+    // Initialize grammar
+    production_rule productions[7] = initProductions();
 
-    // Open file
-    file = openFile(fileName);
+    // Initialize stack
+    Stack stack;
+    StackElement initial_element = {-1, 0}; // symbol, state but why -1?
+    stack.entries[0] = initial_element;     // this is a push?
 
-    // Check if file was successfully opened
-    if (file == NULL)
+    // Initialize tables
+    init_action_table();
+    init_goto_table();
+
+    // Get tokens
+    int tokens[] = {LEFT_PARENTHESIS, NUM, PLUS};
+    int num_token = 3;
+    int current_state;
+
+    for (int i = 0; i < num_token; i++)
     {
-        return "error";
-    }
-
-    // Read file
-    fileContent = readFile(file);
-
-    return fileContent;
-}
-
-void outputMessage(char *outFileName)
-{
-
-    printf("\n");
-    printf("-------------------------------------------------------------\n");
-    printf("-------- Preprocesed file saved in %s --------\n", outFileName);
-    printf("-------------------------------------------------------------\n");
-}
-
-void flagHelp()
-{
-    printf("\nPreprocessor app usage:\n");
-    printf("\t./preprocessor [flags] [filename]\n");
-    printf("Available flags:\n");
-    printf("\t-c : Eliminates comments (default)\n");
-    printf("\t-d : Replaces all directives starting with #\n");
-    printf("\t-all : Does all processing implemented\n");
-    printf("\t-help : Prints this help message\n");
-}
-
-int main(int argc, char **argv)
-{
-    // Variables
-    char *fileName, *outFileName;
-    char *flag1 = NULL, *flag2 = NULL, *flag3 = NULL;
-    char *preprocesedContent;
-    int fd;
-
-    // Get file name from command line argument
-
-    if (argc == 2)
-    {
-        // Check if help flag was provided
-        if (strcmp(argv[1], "-help") == 0)
+        current_state = getTopStack(stack).state;
+        Action current_action = action_table[current_state][tokens[i]];
+        if (current_action.type == REDUCE)
         {
-            flag1 = "-help";
+            reduce(productions[current_action.state_or_production], stack);
+            // pop num
+            // push (f,0)
+        }
+        else if (current_action.type == SHIFT)
+        {
+            shift(stack, tokens[i], current_action.state_or_production);
+            continue;
+        }
+        else if (current_action.type == ACCEPT)
+        {
+            printf("DFA is done\n");
         }
         else
         {
-            fileName = argv[1];
+            printf("Error\n");
         }
     }
-
-    else if (argc == 3)
-        flag1 = argv[1], fileName = argv[2];
-    else if (argc == 4)
-        flag1 = argv[1], flag2 = argv[2], fileName = argv[3];
-    else if (argc == 5)
-        flag1 = argv[1], flag2 = argv[2], flag3 = argv[3], fileName = argv[4];
-
-    // Open file
-
-    // Read file
-
-    if (flag1 == NULL || (strcmp(flag1, "-c") == 0 && flag2 == NULL && flag3 == NULL))
-    {
-        // set up fileContent
-        if (strcmp(setUpfileContent(fileName), "error") == 0)
-        {
-            flagHelp();
-            return 1;
-        }
-        fileContent = setUpfileContent(fileName);
-
-        // Remove comments
-        preprocesedContent = removeComments(fileContent);
-
-        // write prepcocesed content to file
-        outFileName = writeFile(fileName, preprocesedContent);
-
-        // Print output message
-        outputMessage(outFileName);
-
-        // Free memory and close file
-        free(fileContent);
-
-        // Close file
-        fclose(file);
-    }
-    else if (strcmp(flag1, "-d") == 0 && flag2 == NULL && flag3 == NULL)
-    {
-
-        // set up fileContent
-        fileContent = setUpfileContent(fileName);
-
-        // Remove directives
-        preprocesedContent = directivesInclude(fileContent);
-        preprocesedContent = directivesDefine(preprocesedContent);
-        preprocesedContent = directivesIfdef(preprocesedContent);
-        printf("%s", preprocesedContent);
-
-        // write prepcocesed content to file
-        outFileName = writeFile(fileName, preprocesedContent);
-
-        // Print output message
-        outputMessage(outFileName);
-
-        // Free memory and close file
-        free(fileContent);
-
-        // Close file
-        fclose(file);
-    }
-    else if (strcmp(flag1, "-all") == 0 || (strcmp(flag1, "-c") == 0 && flag2 != NULL && strcmp(flag2, "-d") == 0) || (strcmp(flag1, "-d") == 0 && flag2 != NULL && strcmp(flag2, "-c") == 0) || (flag2 != NULL && strcmp(flag2, "-all") == 0) || (flag3 != NULL && strcmp(flag3, "-all") == 0))
-    {
-        // set up fileContent
-        fileContent = setUpfileContent(fileName);
-
-        // Remove all directives
-        preprocesedContent = directivesInclude(fileContent);
-        preprocesedContent = directivesDefine(preprocesedContent);
-        preprocesedContent = directivesIfdef(preprocesedContent);
-        preprocesedContent = removeComments(preprocesedContent);
-
-        // write prepcocesed content to file
-        outFileName = writeFile(fileName, preprocesedContent);
-
-        // Print output message
-        outputMessage(outFileName);
-
-        // Free memory and close file
-        free(fileContent);
-
-        // Close file
-        fclose(file);
-    }
-    else if (strcmp(flag1, "-help") == 0)
-    {
-        flagHelp();
-    }
-    else
-    {
-        printf("Invalid flag\n\n");
-        flagHelp();
-    }
-
     return 0;
 }
