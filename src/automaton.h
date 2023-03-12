2ee30ecb0df17bc6bacd7323d403a148d74169c6
RobinPmrd <robinpmrd@gmail.com>
2023-03-12 17:23:49 +0100

    Improvement the writing in the output file

--- src/automaton.h
+++ src/automaton.h
@@ -11,7 +11,8 @@ void printReduce(FILE* output_file, production_rule production, int new_state, S
     {
         fprintf(output_file, " ");
     }
-    fprintf(output_file, "%d           ", new_state);
+    if (new_state < 10) fprintf(output_file, "%d           ", new_state);
+    else fprintf(output_file, "%d          ", new_state);
     printStack(*stack, output_file);
 }
 
@@ -33,7 +34,8 @@ void reduce(production_rule production, Stack *stack, int** goto_table, FILE *ou
  * Write the shift action and the updated stack content in the outputfile
 */
 void printShift(FILE* output_file, Stack *stack, int next_state) {
-    fprintf(output_file, "   SHIFT               %d           ", next_state);
+    if (next_state < 10) fprintf(output_file, "   SHIFT               %d           ", next_state);
+    else fprintf(output_file, "   SHIFT               %d          ", next_state);
     printStack(*stack, output_file);
 }
 
@@ -42,6 +44,11 @@ void shift(Stack *stack, char token_lexeme[], int next_state, FILE *output_file)
     StackElement *new_element = createStackElement(token_lexeme, next_state);
     push(stack, new_element);
     printShift(output_file, stack, next_state);
+}
+
+void printStateInput(FILE* output_file, int current_state, char token_lexeme[] ) {
+    if (current_state < 10) fprintf(output_file, "      %d                  %s           ", current_state, token_lexeme);
+    else fprintf(output_file, "      %d                 %s           ", current_state, token_lexeme);
 }
 
 void runAutomaton(production_rule* productions, Action** action_table, int** goto_table, Token tokens[], int num_tokens, FILE *output_file) {
@@ -54,7 +61,7 @@ void runAutomaton(production_rule* productions, Action** action_table, int** got
     while (i < num_tokens)
     {
         current_state = peek(stack).state;
-        fprintf(output_file, "      %d                  %s           ", current_state, tokens[i].lexeme);
+        printStateInput(output_file, current_state, tokens[i].lexeme);
         Action current_action = action_table[current_state][tokens[i].index];
         if (current_action.type == REDUCE)
         {
